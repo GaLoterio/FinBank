b2f0792663f412a76e30ee947dfb7313cb43d176
Gabriel Loterio <loteriogabriel05@gmail.com>
2026-05-15 10:48:47 -0300

    chore: project setup

---
+++ src/main/java/com/gabrielloterio/FinBank/FinBankApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielloterio.FinBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinBankApplication.class, args);
+	}
+
+}
